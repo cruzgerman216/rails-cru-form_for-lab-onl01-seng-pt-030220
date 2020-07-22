9684193ec1d03342a41d255c61c8b6a89684b6e6
German Cruz <cruzgerman216@yahoo.com>
2020-07-22 18:24:19 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,6 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
   resources :artists, only: [:index, :new, :show, :create, :edit, :update]
   resources :genres, only: [:index, :new, :show, :create, :edit, :update]
   resources :songs, only: [:index, :new, :show, :create, :edit, :update]
 
-=======
-  resources :artists
->>>>>>> 682f38486bc3ab24abd91c5ea6102f645039fb53
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
